6e4be2b9e78f1b73e2095b978fa826c6505d04e2
tim <cuthbert.tim@hotmail.com>
2022-10-17 23:13:36 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { map, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'base-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  small$ = this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small]).pipe(map(({ matches }) => matches));
+  destroyed$ = new Subject<void>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.watchBreakpoint();
+  }
+
+  private watchBreakpoint() {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(result => {});
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-const moduleImports = [BrowserModule, BrowserAnimationsModule, AppRoutingModule];
+import { LayoutModule } from '@angular/cdk/layout';
+const moduleImports = [BrowserModule, BrowserAnimationsModule, AppRoutingModule, LayoutModule];
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-const materialImports = [MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule];
+import { MatButtonModule } from '@angular/material/button';
+const materialImports = [MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule, MatButtonModule];
 
 import { AppComponent } from './app.component';
 
