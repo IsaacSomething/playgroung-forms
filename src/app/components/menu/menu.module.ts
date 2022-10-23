4acf39a7672ddcb2ae8b0f83c24de4472281866a
tim <cuthbert.tim@hotmail.com>
2022-10-23 12:16:43 +0200

    + router in menu

--- src/app/components/menu/menu.module.ts
+++ src/app/components/menu/menu.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
+const moduleImports = [RouterModule];
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
@@ -11,6 +14,6 @@ import { MenuComponent } from './menu.component';
 @NgModule({
   declarations: [MenuComponent],
   exports: [MenuComponent],
-  imports: [CommonModule, ...materialImports]
+  imports: [CommonModule, ...materialImports, ...moduleImports]
 })
 export class MenuModule {}
